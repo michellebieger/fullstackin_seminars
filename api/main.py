578a25a59c619047c0f7fdca320bb408d49aed7d
michellebieger <michellebieger@live.com>
2023-02-17 16:14:08 +0000

    adding in template and JS info

--- api/main.py
+++ api/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from . import settings
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request): 
