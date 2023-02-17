14f5be2144424d79b5e68b28f12339f3a0c39f90
michellebieger <michellebieger@live.com>
2023-02-17 15:51:32 +0000

    adding in main api pages

---
+++ api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request): 
+    return settings.TEMPLATES.TemplateResponse("homepage.html",{"request": request})
+
+@app.get("/hello")
+async def hello(): 
+    return f"Hello,world!"
+
+@app.get("/hello/{name}")
+async def hello_name(name: str): 
+    return f"Hello,{name}"
